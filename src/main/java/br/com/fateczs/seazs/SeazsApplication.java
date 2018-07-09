7acf5a3f8537f0e583927fa2862832bc9d003a93
Daniel Soares <danielcs1993@hotmail.com>
2018-07-09 14:05:12 -0300

    Setup Inicial do Projeto

---
+++ src/main/java/br/com/fateczs/seazs/SeazsApplication.java
@@ -0,0 +1,12 @@
+package br.com.fateczs.seazs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeazsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeazsApplication.class, args);
+	}
+}
