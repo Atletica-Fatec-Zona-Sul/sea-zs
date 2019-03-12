d3a783b83fc52d18310fa04e64ba231cfdb79ffa
gsvalgreen <gustavo.valverde5@gmail.com>
2019-03-11 21:59:39 -0300

    (WIP) Construção das consultas de pontuação (gustavo.valverde5@gmail.com)

--- src/main/java/br/com/fateczs/seazs/model/RodapeRelatorioPontuacao.java
+++ src/main/java/br/com/fateczs/seazs/model/RodapeRelatorioPontuacao.java
@@ -1,4 +1,28 @@
 package br.com.fateczs.seazs.model;
 
 public class RodapeRelatorioPontuacao {
+
+    private int totalDeUsuarios;
+    private int totalDeAtividades;
+
+    public RodapeRelatorioPontuacao(int totalDeUsuarios, int totalDeAtividades) {
+        this.totalDeUsuarios = totalDeUsuarios;
+        this.totalDeAtividades = totalDeAtividades;
+    }
+
+    public int getTotalDeUsuarios() {
+        return totalDeUsuarios;
+    }
+
+    public void setTotalDeUsuarios(int totalDeUsuarios) {
+        this.totalDeUsuarios = totalDeUsuarios;
+    }
+
+    public int getTotalDeAtividades() {
+        return totalDeAtividades;
+    }
+
+    public void setTotalDeAtividades(int totalDeAtividades) {
+        this.totalDeAtividades = totalDeAtividades;
+    }
 }
